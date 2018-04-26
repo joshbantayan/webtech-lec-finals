05aebb2f0999e137b4e575e20feed4a558d5f818
NavarroNorielJon <2161880@slu.edu.ph>
2018-04-27 00:58:41 +0800

    Added content on login.html

--- connect/connect.php
+++ connect/connect.php
@@ -7,4 +7,8 @@ $dbName = "userlogin";
 
 $connection = mysqli_connect($dbServer, $dbUser, $dbPassword, $dbName);
 
+if($connection->connect_error){
+    die("Connection to database error: " . $connection->connect_error)
+}
+    echo "Connection Success";
 ?>
